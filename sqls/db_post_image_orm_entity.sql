ef36cdfd2e7ad54b4085943309748b4fb92cc12d
MunSeongyun <borygashill608@gmail.com>
2025-05-28 02:26:53 +0900

    안내글들 추가

--- sqls/db_post_image_orm_entity.sql
+++ sqls/db_post_image_orm_entity.sql
@@ -30,7 +30,7 @@ CREATE TABLE `post_image_orm_entity` (
   PRIMARY KEY (`id`),
   KEY `FK_fc05634b51acfe8a0ac79ee1c69` (`postId`),
   CONSTRAINT `FK_fc05634b51acfe8a0ac79ee1c69` FOREIGN KEY (`postId`) REFERENCES `post_orm_entity` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `post_image_orm_entity` (
 
 LOCK TABLES `post_image_orm_entity` WRITE;
 /*!40000 ALTER TABLE `post_image_orm_entity` DISABLE KEYS */;
-INSERT INTO `post_image_orm_entity` VALUES (1,11,'/dummy/test-news-image.png',1470501),(2,12,'/dummy/test-news-image.png',1470501),(3,13,'/dummy/test-news-image.png',1470501),(4,14,'/dummy/test-news-image.png',1470501),(5,15,'/dummy/test-news-image.png',1470501),(6,16,'/dummy/test-news-image.png',1470501),(7,17,'/dummy/test-news-image.png',1470501),(8,18,'/dummy/test-news-image.png',1470501),(9,19,'/dummy/test-news-image.png',1470501),(10,20,'/dummy/test-news-image.png',1470501),(11,51,'/dummy/test-news-image.png',1470501),(12,52,'/dummy/test-news-image.png',1470501),(13,53,'/dummy/test-news-image.png',1470501),(14,54,'/dummy/test-news-image.png',1470501),(15,55,'/dummy/test-news-image.png',1470501),(16,56,'/dummy/test-news-image.png',1470501),(17,57,'/dummy/test-news-image.png',1470501),(18,58,'/dummy/test-news-image.png',1470501),(19,59,'/dummy/test-news-image.png',1470501),(20,60,'/dummy/test-news-image.png',1470501),(21,91,'/dummy/test-news-image.png',1470501),(22,92,'/dummy/test-news-image.png',1470501),(23,93,'/dummy/test-news-image.png',1470501),(24,94,'/dummy/test-news-image.png',1470501),(25,95,'/dummy/test-news-image.png',1470501),(26,96,'/dummy/test-news-image.png',1470501),(27,97,'/dummy/test-news-image.png',1470501),(28,98,'/dummy/test-news-image.png',1470501),(29,99,'/dummy/test-news-image.png',1470501),(30,100,'/dummy/test-news-image.png',1470501),(31,140,'/post-image/02bf946d-71c9-4398-98ca-75768e47a0fa-applicants.png',87799),(32,141,'/post-image/c67e2034-123e-48ad-952d-8de9df9c4016-guidelinesForApplicants.png',57118);
+INSERT INTO `post_image_orm_entity` VALUES (1,11,'/dummy/test-news-image.png',1470501),(2,12,'/dummy/test-news-image.png',1470501),(3,13,'/dummy/test-news-image.png',1470501),(4,14,'/dummy/test-news-image.png',1470501),(5,15,'/dummy/test-news-image.png',1470501),(6,16,'/dummy/test-news-image.png',1470501),(7,17,'/dummy/test-news-image.png',1470501),(8,18,'/dummy/test-news-image.png',1470501),(9,19,'/dummy/test-news-image.png',1470501),(10,20,'/dummy/test-news-image.png',1470501),(11,51,'/dummy/test-news-image.png',1470501),(12,52,'/dummy/test-news-image.png',1470501),(13,53,'/dummy/test-news-image.png',1470501),(14,54,'/dummy/test-news-image.png',1470501),(15,55,'/dummy/test-news-image.png',1470501),(16,56,'/dummy/test-news-image.png',1470501),(17,57,'/dummy/test-news-image.png',1470501),(18,58,'/dummy/test-news-image.png',1470501),(19,59,'/dummy/test-news-image.png',1470501),(20,60,'/dummy/test-news-image.png',1470501),(21,91,'/dummy/test-news-image.png',1470501),(22,92,'/dummy/test-news-image.png',1470501),(23,93,'/dummy/test-news-image.png',1470501),(24,94,'/dummy/test-news-image.png',1470501),(25,95,'/dummy/test-news-image.png',1470501),(26,96,'/dummy/test-news-image.png',1470501),(27,97,'/dummy/test-news-image.png',1470501),(28,98,'/dummy/test-news-image.png',1470501),(29,99,'/dummy/test-news-image.png',1470501),(30,100,'/dummy/test-news-image.png',1470501),(31,140,'/post-image/02bf946d-71c9-4398-98ca-75768e47a0fa-applicants.png',87799),(32,141,'/post-image/c67e2034-123e-48ad-952d-8de9df9c4016-guidelinesForApplicants.png',57118),(33,1,'//post-image/fa2a9148-b9a5-495c-b8a2-125516135ed2-20250502-103309_67dec0e0-26f5-11f0-9604-bb5507ce181f.png',1967989);
 /*!40000 ALTER TABLE `post_image_orm_entity` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-22  0:48:03
+-- Dump completed on 2025-05-28  2:25:54
