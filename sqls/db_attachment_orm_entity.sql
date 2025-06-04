f583f47e00fb157bc929bc8f1f9a70d0dea1baeb
MunSeongyun <borygashill608@gmail.com>
2025-06-04 09:51:31 +0900

    0604 파일, db 수정

--- sqls/db_attachment_orm_entity.sql
+++ sqls/db_attachment_orm_entity.sql
@@ -30,7 +30,7 @@ CREATE TABLE `attachment_orm_entity` (
   PRIMARY KEY (`id`),
   KEY `FK_5831c8d770ea525c5a44613a329` (`postId`),
   CONSTRAINT `FK_5831c8d770ea525c5a44613a329` FOREIGN KEY (`postId`) REFERENCES `post_orm_entity` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-28  9:39:34
+-- Dump completed on 2025-06-04  9:49:41
